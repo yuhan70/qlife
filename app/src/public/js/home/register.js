bfa4967ec9c797e3842130514b0d36d79daf7789
yuhan Jeong <dongi70@naver.com>
2022-03-19 07:34:22 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,20 +1,28 @@
 "use strict";
 
 const id = document.querySelector("#id"),
+    name = document.querySelector("#name"),
     psword = document.querySelector("#psword"), 
-    loginBtn = document.querySelector("#button");
+    confirmPsword = document.querySelector("#confirm-psword"), 
+    registerBtn = document.querySelector("#button");
 
-loginBtn.addEventListener("click",login);
+console.log("Hello Register");
 
-function login(){
+registerBtn.addEventListener("click",register); 
+function register(){
    
     console.log(id.value);
     const req ={
         id: id.value,
+        name :name.value,
         psword: psword.value,
+        confirmPsword : confirmPsword.value,
     };
-  
-    fetch("/login" , {
+   
+    console.log(req);
+
+
+    fetch("/register" , {
         method : "POST",     // REST API 의 경우에 
         headers:{
            "Content-Type":"application/json",
@@ -24,14 +32,14 @@ function login(){
     .then((res) =>res.json())             // 서버에서 응답한 데이터를 받기 위해서 . then 사용
     .then((res) => {
          if(res.success){
-             location.href = "/";
+             location.href = "/login";
          }else{
              alert(res.msg)              // 서버에서 전달한 메시지를 alert으로 보냄.
          }           
     }  )
    .catch((err)=>{
       // console.error(new Error("로그인 중 에러 발생"));
-      console.error("로그인 중 에러 발생");
+      console.error("회원가입 중 에러 발생");
    });
     
      
